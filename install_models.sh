2ae60ea1f17bf59c39b9cc3c9a3948a03e43ba68
RomanRobotnik <rnavarro@robotnik.es>
2019-05-22 13:39:01 +0200

    Update install script to install ar correctly

--- install_models.sh
+++ install_models.sh
@@ -2,6 +2,12 @@
 
 echo "ROBOTNIK GAZEBO MODELS INSTALLATION"
 mkdir -p ~/.gazebo/models
-cp -r . ~/.gazebo/models/
+
+cp -r ./ar_tags/* ~/.gazebo/models/
+cp -r ./cart_1 ~/.gazebo/models/
+cp -r ./fhag ~/.gazebo/models/
+cp -r ./rb2_simple_cart ~/.gazebo/models/
+cp -r ./rb1_docking_station_1 ~/.gazebo/models/
+cp -r ./two_reflector_marker3 ~/.gazebo/models/
 
 echo "Complete!!!"
