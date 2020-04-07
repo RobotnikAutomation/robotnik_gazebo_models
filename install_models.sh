5866639f68e9d568071208ed1d4a687e3611a71f
David <dredo@robotnik.es>
2020-04-07 15:24:20 +0200

    Changed installation script

--- install_models.sh
+++ install_models.sh
@@ -4,13 +4,7 @@ echo "ROBOTNIK GAZEBO MODELS INSTALLATION"
 mkdir -p ~/.gazebo/models
 
 cp -r ./ar_tags/* ~/.gazebo/models/
-cp -r ./cart_1 ~/.gazebo/models/
-cp -r ./fhag ~/.gazebo/models/
-cp -r ./rb2_simple_cart ~/.gazebo/models/
-cp -r ./rb1_docking_station_1 ~/.gazebo/models/
-cp -r ./two_reflector_marker3 ~/.gazebo/models/
-cp -r ./rb1_rblog_cart ~/.gazebo/models/
-cp -r ./floor ~/.gazebo/models/
-cp -r ./floor_with_ground/models/
+cp -r . ~/.gazebo/models/
+rm -r ~/.gazebo/models/ar_tags/
 
 echo "Complete!!!"
